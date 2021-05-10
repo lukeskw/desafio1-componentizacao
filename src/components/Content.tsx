53572acec8017a6535f15e78fb22bd74cce39208
Porfirio Dev <lucasporfirioa@gmail.com>
2021-05-10 11:54:46 -0300

    extra challenge completed

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,66 @@
-export function Content() {
+import { useEffect, useState } from 'react'
+import { MovieCard } from './MovieCard';
+import { api } from '../services/api';
+
+import '../styles/global.scss';
+
+
+import '../styles/content.scss';
+
+
+// interface MovieProps {
+//   imdbID: string;
+//   Title: string;
+//   Poster: string;
+//   Ratings: Array<{
+//     Source: string;
+//     Value: string;
+//   }>;
+//   Runtime: string;
+// }
+
+type ChildContentProps = {
+  movies: Array<{
+      imdbID: string;
+      Title: string;
+      Poster: string;
+      Ratings: Array<{
+        Source: string;
+        Value: string;
+      }>;
+      Runtime: string;
+  }>;
+  selectedGenre: { 
+    id: number;
+    title: string;
+  };
+}
+
+export function Content({movies, selectedGenre}:ChildContentProps) {
   // Complete aqui
+  // const [movies, setMovies] = useState<MovieProps[]>([]);
+  // const [selectedGenreId, setSelectedGenreId] = useState(1);
+  // useEffect(() => {
+  //   api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+  //     setMovies(response.data);
+  //   });
+
+   
+  // }, [selectedGenreId]);
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+            <div className="movies-list">
+              {movies.map(movie => (
+                <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+              ))}
+            </div>
+          </main>
+    </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,81 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from 'react'
+
+import { Button } from './Button';
+import { api } from '../services/api';
+import '../styles/sidebar.scss';
+import '../styles/global.scss';
+
+
+
+// interface GenreResponseProps {
+//   id: number;
+//   name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+//   title: string;
+// }
+
+//recebendo child props do app
+type ChildProps = {
+  //https://www.typescriptlang.org/docs/handbook/basic-types.html array type
+  genres: Array<{
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }>,
+  selectedGenreId: number;
+  setSelectedGenreId : ((id: number) => number)
+
+}
+export function SideBar({genres,setSelectedGenreId,selectedGenreId}:ChildProps) {
+    // const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+    // const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+    // const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+
+    // useEffect(() => {
+    //   api.get<GenreResponseProps[]>('genres').then(response => {
+    //     setGenres(response.data);
+    //   });
+    // }, []);
+
+    // useEffect(() => {
+      
+  
+    //   api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
+    //     setSelectedGenre(response.data);
+    //   })
+    // }, [selectedGenreId]);
+  
+
+    function handleClickButton(id: number) {
+      setSelectedGenreId(id);
+    }
+
+    return(
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+
+      {/* <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+      </div> */}
+    </div>
+    )
+
 }
